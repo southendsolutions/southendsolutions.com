346e4949ea3842e5379afb01771dd01798b59f22
Barneyjm <jamesbarney71@gmail.com>
2019-07-18 22:22:15 -0400

    initial tf

---
+++ infrastructure/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  description = "Name of the application"
+  default = "southendsolutions"
+}
+
+variable "route53_zone_id" {
+  description = "Route 53 zone id"
+}
+
+variable "environment" {
+  description = "Environment [dev, pre, prod]"
+}
+
+variable "ssl_arn" {
+  description = "Arn of the ssl certificate"
+}
+
+variable "url" {
+  description = "Url of the app"
+}
+
+variable "cloudfront_origin_path" {
+  description = "Root path in the bucket for the cloudfront"
+  default     = ""
+}
+
+variable "region" {
+  description = "Region for the bucket"
+  default     = "eu-west-1"
+}
